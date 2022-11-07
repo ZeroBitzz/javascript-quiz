ec22c9ca5bb49577541024469c7d45051da2ab82
Broderick <zerothesu@gmail.com>
2022-11-07 04:48:46 -0700

    added initial screen with style and script that removes title screen

---
+++ assests/scripts/script.js
@@ -0,0 +1,6 @@
+let initialButton = document.getElementById("initial-button")
+initialButton.addEventListener("click", jsGame)
+
+function jsGame(){
+    document.getElementById("initial-screen-section").style.display = "none"
+}
