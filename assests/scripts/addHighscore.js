e3aa23aa5142a65698f5f8b89227402a2df13a09
Broderick <zerothesu@gmail.com>
2022-11-14 22:13:23 -0700

    the scores drop lower when you get a higher score, ex. I got 3rd, so now 4th drops to 5th, and 5th disapears

--- assests/scripts/addHighscore.js
+++ assests/scripts/addHighscore.js
@@ -1,10 +1,10 @@
 //IMPORTS
-import { endTime, endScore } from "./main.js"
+import { endScore, endTime } from "./main.js"
 import { updateScores } from "./updateScores.js"
 // localStorage.clear()
 let playerScore
-let time
-export function addHighscore(score=endScore, time=endTime, name='John Doe'){
+let playerTime
+export function addHighscore(){
     //SETS VALUES FOR HIGHSCORE1 
     localStorage.getItem('score1') === null ? localStorage.setItem('score1', 0) : null
     localStorage.getItem('time1') === null ? localStorage.setItem('time1', 0) : null
@@ -28,6 +28,8 @@ export function addHighscore(score=endScore, time=endTime, name='John Doe'){
 
     let initialsInput = document.getElementById("initials-input")
     let initials = initialsInput.value
+    playerScore = endScore
+    playerTime = endTime
     let statusElement = document.getElementById("status-element")
     let highscoreElm1 = document.getElementById("highscore1")
     let highscoreElm2 = document.getElementById("highscore2")
@@ -39,54 +41,97 @@ export function addHighscore(score=endScore, time=endTime, name='John Doe'){
     let score3 = localStorage.getItem('score3')
     let score4 = localStorage.getItem('score4')
     let score5 = localStorage.getItem('score5')
-    let oldScore
-    let oldTime
-    let oldName
 
     if(localStorage.getItem('scoreEntered') === 'no'){
-        if(score === 0){
+        if(endScore === 0){
             statusElement.innerHTML = "You can't enter a score when you got zero correct."
         }else if(initials === ''){
             statusElement.innerHTML = "Please enter a name/initials."
         }else{
             localStorage.setItem('scoreEntered', 'yes') //this is so the score can only be entered once every time you play the game
-            if(score1 <= score){
-                if(localStorage.getItem('time1') < time / 10){
-                    localStorage.setItem('score1', score)
-                    localStorage.setItem('time1', time / 10)
+            if(score1 <= endScore){
+                if(localStorage.getItem('time1') < playerTime / 10){
+                    localStorage.setItem('score5', localStorage.getItem('score4'))
+                    localStorage.setItem('time5', localStorage.getItem('time4'))
+                    localStorage.setItem('initials5', localStorage.getItem('initials4'))
+                    highscoreElm5.innerHTML = `#5 - ${localStorage.getItem('initials5')} / score: ${localStorage.getItem('score5')} / time: ${localStorage.getItem('time5')} seconds`
+
+                    localStorage.setItem('score4', localStorage.getItem('score3'))
+                    localStorage.setItem('time4', localStorage.getItem('time3'))
+                    localStorage.setItem('initials4', localStorage.getItem('initials3'))
+                    highscoreElm4.innerHTML = `#4 - ${localStorage.getItem('initials4')} / score: ${localStorage.getItem('score4')} / time: ${localStorage.getItem('time4')} seconds`
+
+                    localStorage.setItem('score3', localStorage.getItem('score2'))
+                    localStorage.setItem('time3', localStorage.getItem('time2'))
+                    localStorage.setItem('initials3', localStorage.getItem('initials2'))
+                    highscoreElm3.innerHTML = `#3 - ${localStorage.getItem('initials3')} / score: ${localStorage.getItem('score3')} / time: ${localStorage.getItem('time3')} seconds`
+
+                    localStorage.setItem('score2', localStorage.getItem('score1'))
+                    localStorage.setItem('time2', localStorage.getItem('time1'))
+                    localStorage.setItem('initials2', localStorage.getItem('initials1'))
+                    highscoreElm2.innerHTML = `#2 - ${localStorage.getItem('initials2')} / score: ${localStorage.getItem('score2')} / time: ${localStorage.getItem('time2')} seconds`
+
+                    localStorage.setItem('score1', endScore)
+                    localStorage.setItem('time1', playerTime / 10)
                     localStorage.setItem('initials1', initials)
                     highscoreElm1.innerHTML = `#1 - ${localStorage.getItem('initials1')} / score: ${localStorage.getItem('score1')} / time: ${localStorage.getItem('time1')} seconds`
                 }
-            }else if(score2 <= score){
-                if(localStorage.getItem('time2') < time / 10){
-                    oldScore = localStorage.getItem('score2')
-                    oldTime = localStorage.getItem('time2')
-                    oldName = localStorage.getItem('name2')
-                    localStorage.setItem('score2', score)
-                    localStorage.setItem('time2', time / 10)
+            }else if(score2 <= endScore){
+                if(localStorage.getItem('time2') < playerTime / 10){
+                    localStorage.setItem('score5', localStorage.getItem('score4'))
+                    localStorage.setItem('time5', localStorage.getItem('time4'))
+                    localStorage.setItem('initials5', localStorage.getItem('initials4'))
+                    highscoreElm5.innerHTML = `#5 - ${localStorage.getItem('initials5')} / score: ${localStorage.getItem('score5')} / time: ${localStorage.getItem('time5')} seconds`
+
+                    localStorage.setItem('score4', localStorage.getItem('score3'))
+                    localStorage.setItem('time4', localStorage.getItem('time3'))
+                    localStorage.setItem('initials4', localStorage.getItem('initials3'))
+                    highscoreElm4.innerHTML = `#4 - ${localStorage.getItem('initials4')} / score: ${localStorage.getItem('score4')} / time: ${localStorage.getItem('time4')} seconds`
+
+                    localStorage.setItem('score3', localStorage.getItem('score2'))
+                    localStorage.setItem('time3', localStorage.getItem('time2'))
+                    localStorage.setItem('initials3', localStorage.getItem('initials2'))
+                    highscoreElm3.innerHTML = `#3 - ${localStorage.getItem('initials3')} / score: ${localStorage.getItem('score3')} / time: ${localStorage.getItem('time3')} seconds`
+
+                    localStorage.setItem('score2', endScore)
+                    localStorage.setItem('time2', playerTime / 10)
                     localStorage.setItem('initials2', initials)
                     highscoreElm2.innerHTML = `#2 - ${localStorage.getItem('initials2')} / score: ${localStorage.getItem('score2')} / time: ${localStorage.getItem('time2')} seconds`
-                    addHighscore()
                 }
-            }else if(score3 <= score){
-                if(localStorage.getItem('time3') < time / 10){
-                    localStorage.setItem('score3', score)
-                    localStorage.setItem('time3', time / 10)
+            }else if(score3 <= endScore){
+                if(localStorage.getItem('time3') < playerTime / 10){
+                    localStorage.setItem('score5', localStorage.getItem('score4'))
+                    localStorage.setItem('time5', localStorage.getItem('time4'))
+                    localStorage.setItem('initials5', localStorage.getItem('initials4'))
+                    highscoreElm5.innerHTML = `#5 - ${localStorage.getItem('initials5')} / score: ${localStorage.getItem('score5')} / time: ${localStorage.getItem('time5')} seconds`
+
+                    localStorage.setItem('score4', localStorage.getItem('score3'))
+                    localStorage.setItem('time4', localStorage.getItem('time3'))
+                    localStorage.setItem('initials4', localStorage.getItem('initials3'))
+                    highscoreElm4.innerHTML = `#4 - ${localStorage.getItem('initials4')} / score: ${localStorage.getItem('score4')} / time: ${localStorage.getItem('time4')} seconds`
+
+                    localStorage.setItem('score3', endScore)
+                    localStorage.setItem('time3', playerTime / 10)
                     localStorage.setItem('initials3', initials)
                     highscoreElm3.innerHTML = `#3 - ${localStorage.getItem('initials3')} / score: ${localStorage.getItem('score3')} / time: ${localStorage.getItem('time3')} seconds`
                 }
-            }else if(score4 <= score){
-                if(localStorage.getItem('time4') < time / 10){
-                    localStorage.setItem('score4', score)
-                    localStorage.setItem('time4', time / 10)
+            }else if(score4 <= endScore){
+                if(localStorage.getItem('time4') < playerTime / 10){
+                    localStorage.setItem('score5', localStorage.getItem('score4'))
+                    localStorage.setItem('time5', localStorage.getItem('time4'))
+                    localStorage.setItem('initials5', localStorage.getItem('initials4'))
+                    highscoreElm5.innerHTML = `#5 - ${localStorage.getItem('initials5')} / score: ${localStorage.getItem('score5')} / time: ${localStorage.getItem('time5')} seconds`
+                    
+                    localStorage.setItem('score4', endScore)
+                    localStorage.setItem('time4', playerTime / 10)
                     localStorage.setItem('initials4', initials)
                     highscoreElm4.innerHTML = `#4 - ${localStorage.getItem('initials4')} / score: ${localStorage.getItem('score4')} / time: ${localStorage.getItem('time4')} seconds`
                 }
             }
-            else if(score5 <= score){
-                if(localStorage.getItem('time5') < time / 10){
-                    localStorage.setItem('score5', score)
-                    localStorage.setItem('time5', time / 10)
+            else if(score5 <= endScore){
+                if(localStorage.getItem('time5') < playerTime / 10){
+                    localStorage.setItem('score5', endScore)
+                    localStorage.setItem('time5', playerTime / 10)
                     localStorage.setItem('initials5', initials)
                     highscoreElm5.innerHTML = `#5 - ${localStorage.getItem('initials5')} / score: ${localStorage.getItem('score5')} / time: ${localStorage.getItem('time5')} seconds`
                 }
--- assests/scripts/updateScores.js
+++ assests/scripts/updateScores.js
@@ -7,5 +7,9 @@ export function updateScores(){
 
     if(localStorage.getItem('score1') > 0){
         highscore1.innerHTML = `#1 - ${localStorage.getItem('initials1')} / score: ${localStorage.getItem('score1')} / time: ${localStorage.getItem('time1')} seconds`
+        highscore2.innerHTML = `#2 - ${localStorage.getItem('initials2')} / score: ${localStorage.getItem('score2')} / time: ${localStorage.getItem('time2')} seconds`
+        highscore3.innerHTML = `#3 - ${localStorage.getItem('initials3')} / score: ${localStorage.getItem('score3')} / time: ${localStorage.getItem('time3')} seconds`
+        highscore4.innerHTML = `#4 - ${localStorage.getItem('initials4')} / score: ${localStorage.getItem('score4')} / time: ${localStorage.getItem('time4')} seconds`
+        highscore5.innerHTML = `#5 - ${localStorage.getItem('initials5')} / score: ${localStorage.getItem('score5')} / time: ${localStorage.getItem('time5')} seconds`
     }
 }
